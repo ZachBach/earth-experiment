bb09f89628b792c8e38e86b761ca07055b577dcd
Zach Auerbach <auernachzach@gmail.com>
2021-10-27 22:15:05 -0700

    pushing up from local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { Canvas, useFrame } from '@react-three/fiber'
+import * as THREE from "three";
+import earthImg from './earthimg.jpeg'
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const Sphere=()=>{
+   const base=new THREE.TextureLoader().load(earthImg)
+   const ref=useRef()
+   useFrame(() => (ref.current.rotation.x=ref.current.rotation.y += 0.0004 ))
+   return(
+      <mesh visible castShadow ref={ref} position={[0, 0, 3]}>
+      <directionalLight intensity={0.5} />
+      <sphereGeometry attach="geometry" args={[1, 32, 32]} />
+      <meshBasicMaterial
+      
+         map={base}
+         color="white"
+      />
+      </mesh>
+   )
 }
-
-export default App;
+export default function App(){
+   return (
+     <Canvas >
+         <ambientLight />
+         <Sphere />
+      </Canvas>
+   );
+};
