7f58acf06a7c4b65f46d08d58c6eb12d1e7c8155
Zach Auerbach <auernachzach@gmail.com>
2022-08-19 14:35:27 -0700

    Created a new landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame } from '@react-three/fiber'
 import * as THREE from "three";
-import earthImg from './earthimg.jpeg'
+import earthImg from './earth2.jpg'
 import "./App.css";
 
 
@@ -17,16 +17,25 @@ const Sphere=()=>{
       <meshBasicMaterial
       
          map={base}
-         color="white"
+         color="red"
       />
       </mesh>
    )
 }
 export default function App(){
    return (
+      <>
+      <div>
+         <p  className="head-text">Hello Devil World! 
+         <br/>
+         <br/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, est sed vitae reprehenderit illum aliquam neque magnam, autem quidem non minima ut corporis quo. Error natus atque consectetur magni nulla?</p>
+      </div>
      <Canvas >
          <ambientLight />
          <Sphere />
       </Canvas>
+      <p  className="head-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa cum incidunt soluta est mollitia porro explicabo qui, ad, facere velit deserunt in. Minima sint placeat ab aliquam quidem laboriosam tempore.</p>
+       </>
    );
 };
